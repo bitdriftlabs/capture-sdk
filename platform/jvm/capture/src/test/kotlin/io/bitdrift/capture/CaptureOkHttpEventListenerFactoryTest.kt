e2167569ce4af1775dbc8792dc4fa523fbda7eda
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-08-28 12:57:30 -0400

    remove native library load

--- platform/jvm/capture/src/test/kotlin/io/bitdrift/capture/CaptureOkHttpEventListenerFactoryTest.kt
+++ platform/jvm/capture/src/test/kotlin/io/bitdrift/capture/CaptureOkHttpEventListenerFactoryTest.kt
@@ -31,10 +31,6 @@ class CaptureOkHttpEventListenerFactoryTest {
     private val clock: IClock = mock()
     private val logger: ILogger = mock()
 
-    init {
-        CaptureJniLibrary.load()
-    }
-
     @Test
     fun testRequestAndResponseReuseCommonInfo() {
         // ARRANGE
