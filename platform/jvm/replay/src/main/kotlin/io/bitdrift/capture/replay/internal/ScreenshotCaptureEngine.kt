e39ff3f04d8b3f4a30895a04f1fd2b8447870e5c
Miguel Juarez Lopez <miguel@bitdrift.io>
2024-11-13 18:39:28 -0500

    Move logic to fun

--- platform/jvm/replay/src/main/kotlin/io/bitdrift/capture/replay/internal/ScreenshotCaptureEngine.kt
+++ platform/jvm/replay/src/main/kotlin/io/bitdrift/capture/replay/internal/ScreenshotCaptureEngine.kt
@@ -5,6 +5,8 @@ import android.graphics.Bitmap
 import android.os.Build
 import android.util.Log
 import android.view.PixelCopy
+import android.view.View
+import androidx.annotation.RequiresApi
 import io.bitdrift.capture.common.DefaultClock
 import io.bitdrift.capture.common.ErrorHandler
 import io.bitdrift.capture.common.IClock
@@ -33,24 +35,29 @@ internal class ScreenshotCaptureEngine(
     fun captureScreenshot() {
         // TODO(murki): Use BuildVersionChecker after moving it to common module
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            // TODO(murki): Log Internal log to inform of a non supported API level
             return
         }
         val startTimeMs = clock.elapsedRealtime()
         val topView = windowManager.findRootViews().firstOrNull()
         if (topView == null) {
-            // Log empty screenshot on unblock the rust engine caller
-            logger.onScreenshotCaptured(ByteArray(0), ScreenshotCaptureMetrics(0, 0, 0))
+            logEmpty()
             return
         }
+        modernPixelCopySnapshot(topView, startTimeMs)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    private fun modernPixelCopySnapshot(topView: View, startTimeMs: Long) {
         // TODO(murki): Reduce memory footprint by calling setDestinationBitmap() with a Bitmap.Config.RGB_565 instead
         //  of the default of Bitmap.Config.ARGB_8888
         val screenshotRequest = PixelCopy.Request.Builder.ofWindow(topView).build()
+        // TODO(murki): Handle errors
         PixelCopy.request(screenshotRequest, executor) { screenshotResult ->
             if (screenshotResult.status != PixelCopy.SUCCESS) {
                 errorHandler.handleError("PixelCopy operation failed. Result.status=${screenshotResult.status.toStatusText()}", null)
                 Log.w("miguel-Screenshot", "PixelCopy operation failed. Result.status=${screenshotResult.status.toStatusText()}")
-                // Log empty screenshot on unblock the rust engine caller
-                logger.onScreenshotCaptured(ByteArray(0), ScreenshotCaptureMetrics(0, 0, 0))
+                logEmpty()
                 return@request
             }
 
@@ -60,21 +67,26 @@ internal class ScreenshotCaptureEngine(
                 screenshotAllocationByteCount = resultBitmap.allocationByteCount,
                 screenshotByteCount = resultBitmap.byteCount
             )
-            val stream = ByteArrayOutputStream()
-            // Encode bitmap to bytearray while compressing it using JPEG=10 quality to match iOS
-            resultBitmap.compress(Bitmap.CompressFormat.JPEG, 10, stream)
-            resultBitmap.recycle()
-            // TODO (murki): Figure out if there's a more memory efficient way to do this
-            //  see https://stackoverflow.com/questions/4989182/converting-java-bitmap-to-byte-array#comment36547795_4989543 and
-            //  and https://gaumala.com/posts/2020-01-27-working-with-streams-kotlin.html
-            val screenshotBytes = stream.toByteArray()
-            metrics.compressionTimeMs = clock.elapsedRealtime() - startTimeMs - metrics.screenshotTimeMs
-            metrics.compressionByteCount = screenshotBytes.size
-            Log.d("miguel-Screenshot", "Miguel-Finished screenshot operation on thread=${Thread.currentThread().name}, " +
-                    "metrics=$metrics"
-            )
+            val screenshotBytes = compressScreenshot(resultBitmap)
             logger.onScreenshotCaptured(screenshotBytes, metrics)
         }
+    }
+
+    private fun compressScreenshot(resultBitmap: Bitmap): ByteArray {
+        // TODO(murki): Handle errors
+        val stream = ByteArrayOutputStream()
+        // Encode bitmap to bytearray while compressing it using JPEG=10 quality to match iOS
+        resultBitmap.compress(Bitmap.CompressFormat.JPEG, 10, stream)
+        resultBitmap.recycle()
+        // TODO(murki): Figure out if there's a more memory efficient way to do this
+        //  see https://stackoverflow.com/questions/4989182/converting-java-bitmap-to-byte-array#comment36547795_4989543 and
+        //  and https://gaumala.com/posts/2020-01-27-working-with-streams-kotlin.html
+        return stream.toByteArray()
+    }
+
+    private fun logEmpty() {
+        // Log empty screenshot on unblock the rust engine caller
+        logger.onScreenshotCaptured(ByteArray(0), ScreenshotCaptureMetrics(0, 0, 0))
     }
 
     private fun Int.toStatusText(): String {
