d9091450fc3c0445a43b06ec8c973b9f82106ee6
Miguel Juarez Lopez <miguel@bitdrift.io>
2024-11-14 17:56:00 -0500

    fixes

--- platform/jvm/replay/src/main/kotlin/io/bitdrift/capture/replay/IScreenshotLogger.kt
+++ platform/jvm/replay/src/main/kotlin/io/bitdrift/capture/replay/IScreenshotLogger.kt
@@ -1,7 +1,5 @@
 package io.bitdrift.capture.replay
 
-import kotlin.time.Duration
-
 interface IScreenshotLogger : IInternalLogger {
     fun onScreenshotCaptured(compressedScreen: ByteArray, metrics: ScreenshotCaptureMetrics)
 }
