cbf7f3a44b2806b9d36552da9a0815098ff4e7e4
Snow Pettersen <snowp@lyft.com>
2024-12-04 14:44:26 -0800

    fix license for test code

--- platform/jvm/capture-plugin/src/test/kotlin/io/bitdrift/capture/instrumentation/fakes/TestSpanAddingParameters.kt
+++ platform/jvm/capture-plugin/src/test/kotlin/io/bitdrift/capture/instrumentation/fakes/TestSpanAddingParameters.kt
@@ -1,3 +1,10 @@
+// capture-sdk - bitdrift's client SDK
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
 /**
  * Adapted from https://github.com/getsentry/sentry-android-gradle-plugin/tree/4.14.1
  *
