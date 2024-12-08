168ad6c36a0141bcb2ea52d28008b10203bbf755
Snow Pettersen <snowp@lyft.com>
2024-12-08 09:25:08 +0800

    specify-pom

--- platform/jvm/capture-plugin/build.gradle.kts
+++ platform/jvm/capture-plugin/build.gradle.kts
@@ -36,10 +36,45 @@ mavenPublishing {
     configureBasedOnAppliedPlugins()
 
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-}
 
-signing {
-    sign(publishing.publications)
+    signAllPublications()
+
+    pom {
+        name.set("CapturePlugin")
+        description.set("Capture Gradle plugin.")
+        url.set("https://bitdrift.io")
+        licenses {
+            license {
+                name.set("BITDRIFT SOFTWARE DEVELOPMENT KIT LICENSE AGREEMENT")
+                url.set("https://dl.bitdrift.io/sdk/android-maven/io/bitdrift/capture-timber/${findProperty("VERSION_NAME")}/LICENSE.txt")
+                distribution.set("repo")
+            }
+            license {
+                name.set("NOTICE")
+                url.set("https://dl.bitdrift.io/sdk/android-maven/io/bitdrift/capture-timber/${findProperty("VERSION_NAME")}/NOTICE.txt")
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                id.set("bitdriftlabs")
+                name.set("Bitdrift, Inc.")
+                url.set("https://github.com/bitdriftlabs")
+                email.set("info@bitdrift.io")
+            }
+            developer {
+                id.set("murki")
+                name.set("Miguel Angel Juárez López")
+                url.set("https://github.com/murki")
+                email.set("miguel@bitdrift.io")
+            }
+            scm {
+                connection.set("scm:git:git://github.com/bitdriftlabs/capture-sdk.git")
+                developerConnection.set("scm:git:ssh://git@github.com:bitdriftlabs/capture-sdk.git")
+                url.set("https://github.com/bitdriftlabs/capture-sdk")
+            }
+        }
+    }
 }
 
 group = "io.bitdrift"
