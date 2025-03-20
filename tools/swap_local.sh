63151eab585e8f5e74d819bd6482faacd933d584
Snow Pettersen <snow@bitdrift.io>
2025-03-20 13:32:12 -0700

    shellchk

--- tools/swap_local.sh
+++ tools/swap_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Run this to swap all of the deps to a local version for easy development.
-for crate in $(grep bd- Cargo.toml | cut -d' ' -f1); do
+grep bd- Cargo.toml | cut -d' ' -f1 | while read -r crate; do
   /usr/bin/sed -i '' "s/\(${crate}\)[[:space:]]*=.*/\\1\.path = \"\.\.\/shared-core\/\\1\"/g" Cargo.toml
 done
