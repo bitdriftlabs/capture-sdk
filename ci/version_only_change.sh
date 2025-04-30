02a92c5117aff04e08b3ab8b36de6023bac457f4
Snow Pettersen <snow@bitdrift.io>
2025-04-30 09:13:38 -0700

    fix refernce from check_bazel, add debug

--- ci/version_only_change.sh
+++ ci/version_only_change.sh
@@ -19,6 +19,10 @@ previous_revision=$(git merge-base "$base_sha" "$final_revision")
 
 # If the only file that changed was .sdk_version, we don't need to run bazel-diff and just mark it as no changes detected.
 files_changed=$(git diff --name-only "$previous_revision".."$final_revision")
+
+echo "$(echo "$files_changed" | wc -l) files changed between $previous_revision and $final_revision (truncated):"
+echo "$files_changed" | head -n 5
+
 if echo "$files_changed" | grep -q "platform/shared/.sdk_version" && [ "$(echo "$files_changed" | wc -l)" -eq 1 ]; then
   exit 0
 fi
