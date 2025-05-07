5c2cb16c042b95d75f76ec0cbf4b5892a105bb40
Fran Aguilera <fran@bitdrift.io>
2025-05-07 15:27:41 +0200

    Make format

--- platform/jvm/capture/src/main/kotlin/io/bitdrift/capture/Models.kt
+++ platform/jvm/capture/src/main/kotlin/io/bitdrift/capture/Models.kt
@@ -1,7 +1,14 @@
+// capture-sdk - bitdrift's client SDK
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
 package io.bitdrift.capture
 
 /**
- * A type alias for modeling success or failure operations in the Capture SDK.
+ * A monad for modeling success or failure operations in the Capture SDK.
  */
 typealias CaptureResult<V> = Result<V>
 
@@ -48,6 +55,6 @@ sealed class ApiError(
 }
 
 /**
- * Represents a failed operation due to the SDK not being started.
+ * Represents a failed operation due to the SDK not being started
  */
 data object SdkNotStartedError : Error("SDK not started")
