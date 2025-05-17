ef4a22b198ed470ca972a5738218791a4d8df34f
Martin Conte Mac Donell <reflejo@gmail.com>
2025-05-17 12:10:48 -0700

    z

--- platform/swift/source/replay/Replay+SpriteKit.swift
+++ platform/swift/source/replay/Replay+SpriteKit.swift
@@ -8,12 +8,23 @@
 import SpriteKit
 
 extension Replay {
+    /**
+     * This function is used to traverse the view hierarchy of a SpriteKit scene and
+     * collect the frames of all visible nodes. It takes a buffer to store the frames,
+     * the SKView to traverse, the parent position of the nodes, and a clipping rectangle
+     * to limit the area of interest.
+     *
+     * - parameter buffer:         The buffer to store the frames of the nodes.
+     * - parameter view:           The SKView to traverse.
+     * - parameter parentPosition: The parent position of the nodes.
+     * - parameter clipTo:         The clipping rectangle to limit the area of interest.
+     */
     func traverse(into buffer: inout Data, view: SKView, parentPosition: CGPoint, clipTo: CGRect) {
         guard let scene = view.scene else { return }
 
-        var children: [(CGFloat, CGRect)] = []
+        var children: [(z: CGFloat, frame: CGRect)] = []
         var queue = view.scene?.children.map { ($0, 0, 0, 0.0) } ?? []
-        while let (node, index, parentIndex, parentZ) = queue.popLast() {
+        while let (node, index, parentIndex, nodeZ) = queue.popLast() {
             let ignoreNode =
                 (node.alpha < 0.1 || node.isHidden || node.frame.isEmpty || node.frame.isInfinite
                     || node.frame.isNull || node.frame.width < 0.1 || node.frame.height < 0.1)
@@ -32,21 +43,17 @@ extension Replay {
                 frame.origin.y -= frame.size.height * 0.5
 
                 if frame.intersects(clipTo) {
-                    children.append(
-                        (
-                            (parentZ + node.zPosition) * 1000 + CGFloat(parentIndex) * 100 + CGFloat(index),
-                            frame
-                        )
-                    )
+                    let childIndexOffset = CGFloat(parentIndex) * 100 + CGFloat(index)
+                    children.append((z: nodeZ * 1000 + childIndexOffset, frame))
                 }
             }
 
             node.children.enumerated().forEach {
-                queue.append(($1, $0, parentIndex + 1, parentZ + node.zPosition))
+                queue.append(($1, $0, parentIndex + 1, nodeZ + node.zPosition))
             }
         }
 
-        for (_, frame) in children.sorted(by: { a, b in a.0 <= b.0 }) {
+        for (_, frame) in children.sorted(by: { $0.z <= $1.z }) {
             rectToBytes(type: .sprite, buffer: &buffer, frame: frame.intersection(clipTo))
         }
     }
--- platform/swift/source/replay/Replay.swift
+++ platform/swift/source/replay/Replay.swift
@@ -6,6 +6,7 @@
 // https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
 
 import Foundation
+import SpriteKit
 import UIKit
 
 private typealias CategorizerFunction = (UIView, CGRect) -> AnnotatedView?
