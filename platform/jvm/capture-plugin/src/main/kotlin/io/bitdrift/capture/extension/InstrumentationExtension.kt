82708997effb5f08eb5c1e809a7af519eeedef7e
Karl Stenerud <kstenerud@gmail.com>
2025-06-30 09:51:15 +0200

    Make Gradle plugin configurable from build.gradle (#460)
    * Make the plugin configurable via DSL
    
    * Change boolean property proxyOkHttpEventListener to an enum type okHttpInstrumentationType, with options PROXY and OVERWRITE.

--- platform/jvm/capture-plugin/src/main/kotlin/io/bitdrift/capture/extension/InstrumentationExtension.kt
+++ platform/jvm/capture-plugin/src/main/kotlin/io/bitdrift/capture/extension/InstrumentationExtension.kt
@@ -8,17 +8,27 @@
 package io.bitdrift.capture.extension
 
 import javax.inject.Inject
-import org.gradle.api.model.ObjectFactory
+import org.gradle.api.Project
 import org.gradle.api.provider.Property
 
-open class InstrumentationExtension @Inject constructor(objects: ObjectFactory) {
+open class InstrumentationExtension @Inject constructor(project: Project) {
+    private val objects = project.objects
 
-    val enabled: Property<Boolean> = objects.property(Boolean::class.java)
-            .convention(true)
+    val automaticOkHttpInstrumentation: Property<Boolean> = objects.property(Boolean::class.java)
+            .convention(false)
 
     val debug: Property<Boolean> = objects.property(Boolean::class.java).convention(
             false
     )
 
-    val proxyOkHttpEventListener: Property<Boolean> = objects.property(Boolean::class.java).convention(false)
+    val okHttpInstrumentationType: Property<OkHttpInstrumentationType> = objects.property(OkHttpInstrumentationType::class.java).convention(OkHttpInstrumentationType.OVERWRITE)
+
+    enum class OkHttpInstrumentationType {
+        PROXY,
+        OVERWRITE,
+    }
+
+    // Helpers so that these values can be used directly in the DSL
+    val PROXY = OkHttpInstrumentationType.PROXY
+    val OVERWRITE = OkHttpInstrumentationType.OVERWRITE
 }
