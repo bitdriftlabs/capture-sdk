ac40cddf5375f017b0c800ecfa39d4bd58d30704
delisa fuller <dfuller@bitdrift.io>
2025-07-08 11:47:49 +0100

    feat(android): emit app-open metric at start or resume

--- platform/jvm/capture/src/main/kotlin/io/bitdrift/capture/LoggerImpl.kt
+++ platform/jvm/capture/src/main/kotlin/io/bitdrift/capture/LoggerImpl.kt
@@ -370,6 +370,10 @@ internal class LoggerImpl(
         CaptureJniLibrary.setSleepModeEnabled(this.loggerId, sleepMode == SleepMode.ACTIVE)
     }
 
+    internal fun recordAppOpen() {
+        CaptureJniLibrary.recordAppOpen(this.loggerId)
+    }
+
     @JvmName("logFields")
     @Suppress("TooGenericExceptionCaught")
     internal fun log(
