e13a7ba9d20a1c4ad568f3274dcbcffac3a05617
Fran Aguilera <fran@bitdrift.io>
2025-07-31 11:55:10 +0200

    Reuse existing property at invoke

--- platform/jvm/capture/src/main/kotlin/io/bitdrift/capture/attributes/ClientAttributes.kt
+++ platform/jvm/capture/src/main/kotlin/io/bitdrift/capture/attributes/ClientAttributes.kt
@@ -66,7 +66,7 @@ internal class ClientAttributes(
             // Operating system. Always Android for this code path.
             "os" to "Android",
             // The operating system version (e.g. 12.1)
-            "os_version" to Build.VERSION.RELEASE,
+            "os_version" to osVersion,
             // Whether or not the app was in the background by the time the log was fired.
             "foreground" to isForeground(),
             // The version of this package, as specified by the manifest's `versionName` attribute
