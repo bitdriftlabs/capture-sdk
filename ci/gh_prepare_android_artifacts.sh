a0e2cdf14adefe536684f70609a8cfe62ec68ddb
Snow Pettersen <snow@bitdrift.io>
2025-08-14 15:29:55 -0400

    stop building linux/macos shared libraries during release

--- ci/gh_prepare_android_artifacts.sh
+++ ci/gh_prepare_android_artifacts.sh
@@ -10,33 +10,21 @@ function prepare_capture_sdk() {
   echo "+++ Preparing Android Capture SDK artifacts for '$version' version"
 
   pushd "$(mktemp -d)"
-    local -r out_artifacts_dir="android-tmp"
-    readonly dylibs_dir="capture-$version-dylib"
+  local -r out_artifacts_dir="android-tmp"
+  readonly dylibs_dir="capture-$version-dylib"
 
-    mkdir "$out_artifacts_dir"
-    mkdir "$dylibs_dir/"
-    mkdir "$dylibs_dir/darwin_arm64/"
-    mkdir "$dylibs_dir/darwin_x86_64/"
+  mkdir "$out_artifacts_dir"
+  mkdir "$dylibs_dir/"
+  mkdir "$dylibs_dir/darwin_arm64/"
+  mkdir "$dylibs_dir/darwin_x86_64/"
 
-    echo "++ Creating shared dylibs"
+  echo "++ Unzipping Maven Android artifacts"
 
-    mv "$sdk_repo/libcapture.dylib.arm64" "$dylibs_dir/darwin_arm64/libcapture.dylib"
-    mv "$sdk_repo/libcapture.dylib.x86_64" "$dylibs_dir/darwin_x86_64/libcapture.dylib"
-    mv "$sdk_repo/libcapture.so" "$dylibs_dir/"
+  unzip "$sdk_repo/Capture.android.zip" -d "$out_artifacts_dir"
 
-    cp "$sdk_repo/ci/BUILD.shared_libs" "$dylibs_dir"/BUILD
+  echo "++ Creating Android artifacts zip"
 
-    pushd "$dylibs_dir"
-      tar cvf "../$out_artifacts_dir/$dylibs_dir.tar" ./*
-    popd
-
-    echo "++ Unzipping Maven Android artifacts"
-
-    unzip "$sdk_repo/Capture.android.zip" -d "$out_artifacts_dir"
-
-    echo "++ Creating Android artifacts zip"
-
-    (cd "$out_artifacts_dir" && zip -r "$sdk_repo/Capture-$version.android.zip" ./*)
+  (cd "$out_artifacts_dir" && zip -r "$sdk_repo/Capture-$version.android.zip" ./*)
   popd
 }
 
