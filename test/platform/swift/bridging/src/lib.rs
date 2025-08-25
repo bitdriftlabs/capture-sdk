ed170cc86bec58dd25a36fa079e35e1fb7d70a09
Matt Klein <mklein@bitdrift.io>
2025-08-25 16:09:16 -0600

    fix
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- test/platform/swift/bridging/src/lib.rs
+++ test/platform/swift/bridging/src/lib.rs
@@ -147,7 +147,7 @@ unsafe extern "C" fn test_stream_closed(
 /// Helper for validating the expected output from the end to end test in LoggerTest.swift.
 macro_rules! set_string {
   ($log:ident, $msg:ident, $rust_str:expr) => {
-    let () = msg_send![$log, $msg: make_nsstring($rust_str)];
+    let () = msg_send![$log, $msg: make_nsstring($rust_str).unwrap()];
   };
 }
 
@@ -186,16 +186,16 @@ unsafe extern "C" fn next_uploaded_log(uploaded_log: *mut Object) -> bool {
     set_string!(uploaded_log, setSessionID, received_log.session_id());
 
     for (key, value) in received_log.typed_fields() {
-      let key = make_nsstring(&key);
+      let key = make_nsstring(&key).unwrap();
 
       match value {
         StringOrBytes::String(s) => {
-          let value = make_nsstring(&s);
+          let value = make_nsstring(&s).unwrap();
 
           let () = msg_send![uploaded_log, addStringFieldWithKey:key value:value];
         },
         StringOrBytes::SharedString(s) => {
-          let value = make_nsstring(&s);
+          let value = make_nsstring(&s).unwrap();
 
           let () = msg_send![uploaded_log, addStringFieldWithKey:key value:value];
         },
