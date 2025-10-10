cf86631d6dac84c054d33219fd99a4d984322c12
Fran Aguilera <fran@bitdrift.io>
2025-10-10 09:45:55 +0200

    Force android build and integration tests on ci

--- platform/jvm/capture/src/main/kotlin/io/bitdrift/capture/attributes/ClientAttributes.kt
+++ platform/jvm/capture/src/main/kotlin/io/bitdrift/capture/attributes/ClientAttributes.kt
@@ -202,7 +202,6 @@ internal class ClientAttributes(
      */
     companion object {
         // The unique sdk library that can be used for custom reports
-        // TODO(FranAguilera): BIT-5149 Finalize model
         const val SDK_LIBRARY_ID = "io.bitdrift.capture-android"
 
         private const val UNKNOWN_FIELD_VALUE = "unknown"
