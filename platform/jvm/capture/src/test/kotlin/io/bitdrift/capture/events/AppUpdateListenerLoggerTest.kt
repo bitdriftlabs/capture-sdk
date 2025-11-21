6397275b8e2564adabdc30f2e0cfafe09cf151d7
Snow Pettersen <snow@bitdrift.io>
2025-11-21 11:20:46 -0800

    fmt

--- platform/jvm/capture/src/test/kotlin/io/bitdrift/capture/events/AppUpdateListenerLoggerTest.kt
+++ platform/jvm/capture/src/test/kotlin/io/bitdrift/capture/events/AppUpdateListenerLoggerTest.kt
@@ -25,7 +25,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
