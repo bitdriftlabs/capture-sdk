8d7f1936568b5b8ed2840c9acb7bf74a00c26d42
Jackson Hardaker <7596320+jacksonhardaker@users.noreply.github.com>
2026-01-13 16:29:44 -0500

    setup biome: lint, format

--- platform/webview/src/types.ts
+++ platform/webview/src/types.ts
@@ -5,7 +5,7 @@
 // LICENSE file or at:
 // https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
 
-import { type MetricType } from 'web-vitals';
+import type { MetricType } from 'web-vitals';
 
 /**
  * Message types sent from JS to native bridge
