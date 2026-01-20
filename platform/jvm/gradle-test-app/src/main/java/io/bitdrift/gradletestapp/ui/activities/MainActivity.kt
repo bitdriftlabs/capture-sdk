f7d3ff89140280ca490220d9721bbb2de9a1d942
Fran Aguilera <fran@bitdrift.io>
2026-01-20 09:25:53 +0100

    PIP mode

--- platform/jvm/gradle-test-app/src/main/java/io/bitdrift/gradletestapp/ui/activities/MainActivity.kt
+++ platform/jvm/gradle-test-app/src/main/java/io/bitdrift/gradletestapp/ui/activities/MainActivity.kt
@@ -7,10 +7,23 @@
 
 package io.bitdrift.gradletestapp.ui.activities
 
+import android.app.PictureInPictureParams
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.util.Rational
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -43,6 +56,78 @@ class MainActivity : AppCompatActivity() {
         }
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.pipContainer.setContent {
+            PipContent()
+        }
+
+        updatePipParams()
+    }
+
+    @androidx.compose.runtime.Composable
+    private fun PipContent() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "PiP Mode",
+                color = Color.White,
+                fontSize = 24.sp
+            )
+        }
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        enterPipMode()
+    }
+
+    override fun onPictureInPictureModeChanged(
+        isInPictureInPictureMode: Boolean,
+        newConfig: Configuration
+    ) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        updateUiForPipMode(isInPictureInPictureMode)
+    }
+
+    private fun enterPipMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val params = PictureInPictureParams.Builder()
+                .setAspectRatio(Rational(9, 16))
+                .build()
+            enterPictureInPictureMode(params)
+        }
+    }
+
+    private fun updatePipParams() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val params = PictureInPictureParams.Builder()
+                .setAspectRatio(Rational(9, 16))
+                .setAutoEnterEnabled(true)
+                .setSeamlessResizeEnabled(true)
+                .build()
+            setPictureInPictureParams(params)
+        }
+    }
+
+    private fun updateUiForPipMode(isInPipMode: Boolean) {
+        val contentContainer = findViewById<View>(R.id.content_main_container)
+        if (isInPipMode) {
+            supportActionBar?.hide()
+            contentContainer?.visibility = View.GONE
+            binding.pipContainer.visibility = View.VISIBLE
+        } else {
+            binding.pipContainer.visibility = View.GONE
+            contentContainer?.visibility = View.VISIBLE
+            navController.currentDestination?.let { destination ->
+                if (destination.id != R.id.FirstFragment) {
+                    supportActionBar?.show()
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean =
