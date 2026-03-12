86ca9aa486e16d1c8e018c4d1e510d13b0cafe45
Fran Aguilera <fran@bitdrift.io>
2026-03-12 12:59:41 +0100

    Revert "Remove androidx.metrics:metrics-performance (#914)"
    This reverts commit eb84dd9f9bbd77e8c6265145983ee856a6d0569e.

--- platform/jvm/capture/build.gradle.kts
+++ platform/jvm/capture/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation(libs.androidx.startup.runtime)
     implementation(libs.jsr305)
     implementation(libs.gson)
+    implementation(libs.performance)
     implementation(libs.protobuf.kotlinlite)
 
     compileOnly(libs.retrofit)
