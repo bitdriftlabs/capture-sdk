ede7c50f0226b4554d3fa48c70fe5553c8044c85
Fran Aguilera <fran@bitdrift.io>
2026-03-26 13:23:27 +0100

    Remove noisy benchmark tests on emulator

--- platform/jvm/microbenchmark/src/androidTest/java/io/bitdrift/microbenchmark/LogBenchmarkTest.kt
+++ platform/jvm/microbenchmark/src/androidTest/java/io/bitdrift/microbenchmark/LogBenchmarkTest.kt
@@ -72,26 +72,6 @@ class LogBenchmarkTest {
             Capture.Logger.logInfo { LOG_MESSAGE }
         }
     }
-
-    @Test
-    fun logNotMatched5Fields() {
-        startLogger()
-        val fields = buildFieldsMap(5)
-
-        benchmarkRule.measureRepeated {
-            Capture.Logger.logInfo(fields) { LOG_MESSAGE }
-        }
-    }
-
-    @Test
-    fun logNotMatched10Fields() {
-        startLogger()
-        val fields = buildFieldsMap(10)
-
-        benchmarkRule.measureRepeated {
-            Capture.Logger.logInfo(fields) { LOG_MESSAGE }
-        }
-    }
     
     @Test
     fun logNotMatched5000Fields() {
