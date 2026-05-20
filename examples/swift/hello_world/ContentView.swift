5c4ab2d714ebe23ccc5121f2843cbda63eb38a97
ArielDemarco <ademarco@bitdrift.io>
2026-05-20 17:18:17 -0300

    [BIT-8351] Support Xcode 26 Swiftui rendering changes affecting session replay (#995)
    * [WIP] making this work using CALayer tree
    
    * cleanup
    
    * Added tests
    
    * Finished with tests
    
    * Preventing crash in tests when trying to makeKeyAndVisible the uiwindow
    
    * Added the possibility to use test host so test with uiwindows doesn't fail
    
    * Gated to iOS 26

--- examples/swift/hello_world/ContentView.swift
+++ examples/swift/hello_world/ContentView.swift
@@ -46,11 +46,21 @@ struct ContentView: View {
                             )
                             .clipShape(Capsule())
                     }
-                }
+                }.removeToolBarItemGlassStyle()
             }
         }
         .navigationViewStyle(.stack)
         .accentColor(Theme.primary)
         .onAppear { self.loggerCustomer.logAppLaunchTTI() }
+    }
+}
+
+extension ToolbarContent {
+    func removeToolBarItemGlassStyle() -> some ToolbarContent {
+        if #available(iOS 26.0, *) {
+            return self.sharedBackgroundVisibility(.hidden)
+        } else {
+            return self
+        }
     }
 }
