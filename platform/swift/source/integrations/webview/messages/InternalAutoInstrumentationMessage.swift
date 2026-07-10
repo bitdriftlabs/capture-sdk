0c3321fa97d120ab718c0d7bb6998a3dc33911dc
ArielDemarco <demarcoariel.dev@gmail.com>
2026-07-10 19:02:21 -0300

    WebView support

---
+++ platform/swift/source/integrations/webview/messages/InternalAutoInstrumentationMessage.swift
@@ -0,0 +1,26 @@
+// capture-sdk - bitdrift's client SDK
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
+import Foundation
+
+struct InternalAutoInstrumentationMessage: WebViewLoggableMessage, Equatable {
+    let tag: String
+    let v: Int
+    let type: WebViewMessageType
+    let timestamp: Int64
+    let event: String
+
+    func makeLoggingAction(context: WebViewLoggingContext) -> WebViewLoggingAction? {
+        .log(
+            level: .debug,
+            message: "[WebView] instrumented \(event)",
+            fields: makeFields(
+                ("_event", event)
+            )
+        )
+    }
+}
