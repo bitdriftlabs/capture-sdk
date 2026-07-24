1ecb261b0675edd9acd7114f3ebda6f10a25c9da
ArielDemarco <demarcoariel.dev@gmail.com>
2026-07-24 13:12:38 -0300

    Renamed variable in lock to hasBeenConfigured

--- platform/swift/source/integrations/webview/WebViewIntegration.swift
+++ platform/swift/source/integrations/webview/WebViewIntegration.swift
@@ -26,7 +26,7 @@ extension Integration {
 }
 
 final class WebViewIntegration {
-    private var hasSwizzledWebViewInit = Atomic(false)
+    private var hasBeenConfigured = Atomic(false)
     private let underlyingLogger = Atomic<Logging?>(nil)
 
     static let shared: WebViewIntegration = .init()
@@ -39,7 +39,7 @@ final class WebViewIntegration {
             storedLogger = logger
         }
 
-        hasSwizzledWebViewInit.update { hasSwizzled in
+        hasBeenConfigured.update { hasSwizzled in
             guard !hasSwizzled, !disableSwizzling, logger.runtimeValue(.webviewSwizzling) else {
                 return
             }
@@ -62,7 +62,7 @@ extension WebViewIntegration {
     /// Exchanging the method twice should in theory restore the original implementation.
     /// Note: This should only be used in tests.
     func disableWebViewSwizzling() {
-        hasSwizzledWebViewInit.update { hasSwizzled in
+        hasBeenConfigured.update { hasSwizzled in
             guard hasSwizzled else {
                 return
             }
