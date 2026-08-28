84fc1df467adb2c8a8c241d3805ad0ee5ddc1ed5
Miguel Juárez López <miguel@bitdrift.io>
2026-08-28 18:09:39 -0400

    andr;build: use standalone cmdline-tools using JDK 17, drop JDK 8 (#1167)

--- tools/android_sdk_wrapper.sh
+++ tools/android_sdk_wrapper.sh
@@ -4,14 +4,15 @@ set -euo pipefail
 
 script_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly script_root
-readonly android_sdk_version="4333796"
+# shellcheck source=tools/android_toolchain_versions.sh
+source "$script_root/android_toolchain_versions.sh"
 readonly softlink_root_dir="/tmp/bitdrift-android-sdk"
-readonly custom_android_home="$softlink_root_dir/android-sdk-$android_sdk_version-unarchived"
+readonly custom_android_home="$softlink_root_dir/android-sdk-unarchived"
 
 "$script_root/setup_android_sdk.sh"
 
 unset ANDROID_SDK_ROOT
 
 ANDROID_HOME=$custom_android_home \
-  PATH="$custom_android_home/tools/bin:$custom_android_home/platform-tools:$PATH" \
+  PATH="$custom_android_home/cmdline-tools/$android_cmdline_tools_version/bin:$custom_android_home/platform-tools:$PATH" \
   "$@"
