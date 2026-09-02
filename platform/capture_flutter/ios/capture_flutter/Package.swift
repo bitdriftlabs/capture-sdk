3bbbf564ba4136e788899eb97ea1e08b48ef78f6
Fran Aguilera <fran@bitdrift.io>
2026-09-02 09:49:31 +0200

    Revert "Prototyping iOS session replay"
    This reverts commit 59fca61ad28df2dafa7db02cde995b082ba2fc47.

--- platform/capture_flutter/ios/capture_flutter/Package.swift
+++ platform/capture_flutter/ios/capture_flutter/Package.swift
@@ -1,3 +1,10 @@
+// capture-sdk - bitdrift's client SDK
+// Copyright Bitdrift, Inc. All rights reserved.
+//
+// Use of this source code is governed by a source available license that can be found in the
+// LICENSE file or at:
+// https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt
+
 // swift-tools-version: 5.9
 // capture-sdk - bitdrift's client SDK
 // Copyright Bitdrift, Inc. All rights reserved.
@@ -17,14 +24,13 @@ let package = Package(
         .library(name: "capture-flutter", targets: ["capture_flutter"])
     ],
     dependencies: [
-        .package(path: "/Users/faguilera/development/capture-ios")
+        .package(url: "https://github.com/bitdriftlabs/capture-ios.git", from: "0.22.0")
     ],
     targets: [
         .target(
             name: "capture_flutter",
             dependencies: [
-                .product(name: "Capture", package: "capture-ios"),
-                .product(name: "CaptureIntegrations", package: "capture-ios")
+                .product(name: "Capture", package: "capture-ios")
             ]
         ),
     ]
